d2290b989d5b64322c419e3a87615e241c57f583
JoseFrancisc0 <jose.wong@utec.edu.pe>
2022-07-07 14:11:30 -0500

    unittesting skeleton

---
+++ backend/testcases.py
@@ -0,0 +1,81 @@
+import unittest
+
+from server import create_app
+from models import setup_db
+import json
+
+class TestTamboApi(unittest.TestCase):
+    def setUp(self):
+        self.app = create_app()
+        self.client = self.app.test_client
+        self.database_name = 'quispe'
+        self.database_path = 'quospe'
+        setup_db(self.app, self.database_path)
+
+#------------ADMINISTRADORES-----------------#
+def test_register_admin_failed(self):
+    pass
+
+def test_register_admin_success(self):
+    pass
+
+def test_login_admin_failed(self):
+    pass
+
+def test_login_admin_success(self):
+    pass
+
+#---------------EMPLEADOS--------------------#
+def test_create_empleado_failed(self):
+    pass
+
+def test_create_empleado_success(self):
+    pass
+
+def test_get_empleados_failed(self):
+    pass
+
+def test_get_empleados_success(self):
+    pass
+
+def test_update_empleado_failed(self):
+    pass
+
+def test_update_empleado_success(self):
+    pass
+
+def test_delete_empleado_failed(self):
+    pass
+
+def test_delete_empleado_success(self):
+    pass
+
+#-------------------TAREAS-------------------#
+
+def test_create_tarea_failed(self):
+    pass
+
+def test_create_tarea_success(self):
+    pass
+
+def test_get_tareass_failed(self):
+    pass
+
+def test_get_tareas_success(self):
+    pass
+
+def test_update_tareas_failed(self):
+    pass
+
+def test_update_tarea_success(self):
+    pass
+
+#------------ASIGNACION-DE-TAREAS----------#
+
+def test_assign_tarea_failed(self):
+    pass
+
+def test_assign_tarea_success(self):
+    pass
+
+### note to self: gonna update every case one by one
